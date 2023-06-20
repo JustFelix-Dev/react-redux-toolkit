da0035c3a4e116a915f7205271710b2384d6cbd1
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-20 15:53:45 +0100

    react-redux-toolkit

--- src/features/iceCream/IceCreamView.jsx
+++ src/features/iceCream/IceCreamView.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { ordered, restocked } from './iceCreamSlice'
 
 export const IceCreamView = () => {
+    const [ value,setValue ] = useState(1)
     const numOfCreams = useSelector((state)=> state.iceCream.numOfCreams)
     const dispatch = useDispatch()
   return (
          <>
          <h2>Number of IceCream - { numOfCreams } </h2>
          <button onClick={()=> dispatch(ordered())}>Order IceCream</button>
-         <button onClick={()=> dispatch(restocked(7))}>Restock IceCreams</button>
+         <input type="text" inputMode='numeric' defaultValue={value} onChange={(e)=> setValue(parseInt(e.target.value))} />
+         <button onClick={()=> dispatch(restocked(value))}>Restock IceCreams</button>
          </>
   )
 }
--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchUsers } from './userSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 export const UserView = () => {
+    const user = useSelector((state)=> state.user )
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(fetchUsers())
+    },[])
   return (
         <>
           <h2>List all users</h2>
+          { user.loading && <div>Loading..</div> }
+          {!user.loading && user.error ? <div>Error: {user.error}</div> : null}
+          {!user.loading &&  user.users.length ? 
+          (<ul>
+            {
+              user.users.map((user)=>(
+                <li key={user.id}>{user.name}</li>
+              ))
+            }
+          </ul>) : null }
+
         </>
   )
 }
